296149fa691460ae5e0836e35768cddcac94c543
Chris Cherryholme <chrischerryholme@gmail.com>
2018-09-26 00:49:00 -0400

    added lots of new functions to lab 3

--- app/src/main/java/com/example/chris/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/chris/androidlabs/ListItemsActivity.java
@@ -3,15 +3,46 @@ package com.example.chris.androidlabs;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class ListItemsActivity extends Activity {
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        Log.i(ACTIVITY_NAME, "onCreate");
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "onResume()");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "onDestroy()");
+    }
 }
--- app/src/main/java/com/example/chris/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/chris/androidlabs/LoginActivity.java
@@ -1,13 +1,62 @@
 package com.example.chris.androidlabs;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+
+    private Button btnLogin;
+    private String emailAddress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME, "onCreate()");
+        btnLogin = findViewById(R.id.btnLogin);
+        SharedPreferences prefs = getSharedPreferences(emailAddress, MODE_PRIVATE);
+        String emailAddress = prefs.getString("DefaultEmail", "email@domain.com");
+
+
+        Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "onDestory()");
     }
 }
